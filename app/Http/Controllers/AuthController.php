0718a2244ed6086a4e293552b49109a5c16dba0b
FOREST\regina.ayu <regina.wade93@gmail.com>
2022-11-27 16:59:41 +0700

    update auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,10 +86,10 @@ class AuthController extends Controller
                 $user_data = UserLkpp::where('email', $email)
                 ->value('api_token');   
 
-                if($user_data){
-                    $client1 = new \GuzzleHttp\Client();
-                    $response1 = $client1->request('GET', 'https://staging.eling.co.id/autologin/'.$user_data.'');
+                $clientlogin = new \GuzzleHttp\Client();
+                $responseLogin = $clientlogin->request('GET', 'https://staging.eling.co.id/autologin/'.$user_data.'');
 
+                if($responseLogin){
                             return response()->json(array(
                                 'code' => 200,
                                 'data' => [
